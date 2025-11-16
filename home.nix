c9fbd225563c1d1d0b338f3f4751509564a1a465
vs-123 <>
2025-11-16 10:44:00 +0000

    dotfiles: setup bashrc & vimrc

--- home.nix
+++ home.nix
@@ -5,15 +5,22 @@
     lazygit
   ]; 
 
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-    userName = "vs-123";
-    userEmail = "";
-  }; 
+  programs = {
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      userName = "vs-123";
+      userEmail = "";
+    }; 
+  };
 
   home.file = {
-    ".bashrc".source = ./dfs/bashrc;
+    ".bashrc" = ./dfs/bashrc;
+    ".config/vim" = {source = ./dfs/cfg/vim; recursive = true;};
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
   };
 
   home.stateVersion = "25.05"; 
