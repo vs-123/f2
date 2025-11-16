e289aaae28ef696dee8fd2fd4f41fe30a74e866b
vs-123 <>
2025-11-16 11:05:54 +0000

    moved to newer home-manager; configured xfce with xfconf

--- home.nix
+++ home.nix
@@ -9,18 +9,34 @@
     git = {
       enable = true;
       package = pkgs.gitFull;
-      userName = "vs-123";
-      userEmail = "";
+      settings = {
+        user.name = "vs-123";
+        user.email = "";
+      };
     }; 
   };
 
   home.file = {
-    ".bashrc" = ./dfs/bashrc;
+    ".bashrc".source = ./dfs/bashrc;
     ".config/vim" = {source = ./dfs/cfg/vim; recursive = true;};
   };
 
   home.sessionVariables = {
     EDITOR = "vim";
+  };
+
+  xfconf.settings = {
+    keyboards = {
+      "Default/KeyRepeat/Delay" = 200;
+      "Default/KeyRepeat/Rate" = 50;
+    };
+
+    xfce4-keyboard-shortcuts = {
+      "commands/custom/<Alt>s" = "xfce4-appfinder";
+    };
+    xfwm4 = {
+      "general/button_layout" = "CMHS|O";
+    };
   };
 
   home.stateVersion = "25.05"; 
